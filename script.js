e96dca59b868fcb939415ce4c830796d8d69eeaa
izaiasjr <up1.izaiasjr@gmail.com>
2022-06-09 23:49:11 -0300

    meu desafio de JS T3.1

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// Crie uma variável que receba um nome;
+let nome = "Izaias";
+console.log(nome);
+
+// Crie uma variável que receba um número;
+let numero = "14"
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let ano = "2022"
+console.log(ano - numero);
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+2008
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log( "Estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira);
+
